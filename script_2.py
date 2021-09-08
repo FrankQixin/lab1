333c66ade4f23b5bc0a518e8a02da1938325851f
Qixin Dong <qd@ualberta.ca>
2021-09-08 14:06:07 -0600

    2

--- script_2.py
+++ script_2.py
@@ -1,3 +1,9 @@
 import requests
 
 print(requests.get('http://google.com').text)
+url='https://raw.githubusercontent.com/FrankQixin/lab1/master/script_2.py'
+r=requests.get(url).text
+print(r)
+with open ('script_3.py','w') as doc:
+    doc.write(r)
+    doc.close()
